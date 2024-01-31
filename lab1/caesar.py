0549bc3d36fa29e58b34acdf92bba2e48dc6a4eb
Elizabeth <bellator.379@gmail.com>
2024-01-31 23:56:22 +0300

    LW 1 done

---
+++ lab1/caesar.py
@@ -0,0 +1,48 @@
+def caesar_cipher_encrypt(text, shift):
+    encrypted_text = ''
+    for char in text:
+        if char.isalpha():
+            shifted = ord(char) + shift
+            if char.islower():
+                if shifted > ord('z'):
+                    shifted -= 26
+                elif shifted < ord('a'):
+                    shifted += 26
+            elif char.isupper():
+                if shifted > ord('Z'):
+                    shifted -= 26
+                elif shifted < ord('A'):
+                    shifted += 26
+            encrypted_text += chr(shifted)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+
+def caesar_cipher_decrypt(encrypted_text, shift):
+    return caesar_cipher_encrypt(encrypted_text, -shift)
+
+
+def encrypt_file_caesar(input_file, output_file, shift):
+    with open(input_file, 'r') as file:
+        text = file.read()
+    encrypted_text = caesar_cipher_encrypt(text, shift)
+    with open(output_file, 'w') as file:
+        file.write(encrypted_text)
+
+
+def decrypt_file_caesar(input_file, output_file, shift):
+    with open(input_file, 'r') as file:
+        encrypted_text = file.read()
+    decrypted_text = caesar_cipher_decrypt(encrypted_text, shift)
+    with open(output_file, 'w') as file:
+        file.write(decrypted_text)
+
+# Шифр Цезаря
+input_file = 'files/input.txt'
+output_file = 'files/encrypted_caesar.txt'
+shift = 4
+
+encrypt_file_caesar(input_file, output_file, shift)
+
+decrypt_file_caesar(output_file, 'files/decrypted_caesar.txt', shift)
---
+++ lab1/vigenere.py
@@ -0,0 +1,56 @@
+def vigenere_cipher_encrypt(plain_text, key):
+    encrypted_text = ''
+    key_length = len(key)
+    for i, char in enumerate(plain_text):
+        if char.isalpha():
+            key_shift = ord(key[i % key_length].lower()) - ord('a')
+            if char.islower():
+                shifted = (ord(char) - ord('a') + key_shift) % 26 + ord('a')
+            elif char.isupper():
+                shifted = (ord(char) - ord('A') + key_shift) % 26 + ord('A')
+            encrypted_text += chr(shifted)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+
+def vigenere_cipher_decrypt(encrypted_text, key):
+    decrypted_text = ''
+    key_length = len(key)
+    for i, char in enumerate(encrypted_text):
+        if char.isalpha():
+            key_shift = ord(key[i % key_length].lower()) - ord('a')
+            if char.islower():
+                shifted = (ord(char) - ord('a') - key_shift) % 26 + ord('a')
+            elif char.isupper():
+                shifted = (ord(char) - ord('A') - key_shift) % 26 + ord('A')
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+
+def encrypt_file_vigenere(input_file, output_file, key):
+    with open(input_file, 'r') as file:
+        text = file.read()
+    encrypted_text = vigenere_cipher_encrypt(text, key)
+    with open(output_file, 'w') as file:
+        file.write(encrypted_text)
+
+
+def decrypt_file_vigenere(input_file, output_file, key):
+    with open(input_file, 'r') as file:
+        encrypted_text = file.read()
+    decrypted_text = vigenere_cipher_decrypt(encrypted_text, key)
+    with open(output_file, 'w') as file:
+        file.write(decrypted_text)
+
+
+# Шифр Виженера
+input_file = 'files/input.txt'
+output_file = 'files/encrypted_vigenere.txt'
+key = 'key'
+
+encrypt_file_vigenere(input_file, output_file, key)
+
+decrypt_file_vigenere(output_file, 'files/decrypted_vigenere.txt', key)
