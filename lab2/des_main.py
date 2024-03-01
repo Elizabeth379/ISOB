28c15da68adf8b1dc170a7581cb8458fbe98e247
Elizabeth <bellator.379@gmail.com>
2024-03-01 21:55:11 +0300

    Lab 2 done

--- lab2/des_main.py
+++ lab2/des_main.py
@@ -1,4 +1,4 @@
-# Initial permut matrix for the datas
+# Начальная матрица перестановки для данных
 PI = [58, 50, 42, 34, 26, 18, 10, 2,
       60, 52, 44, 36, 28, 20, 12, 4,
       62, 54, 46, 38, 30, 22, 14, 6,
@@ -8,7 +8,7 @@ PI = [58, 50, 42, 34, 26, 18, 10, 2,
       61, 53, 45, 37, 29, 21, 13, 5,
       63, 55, 47, 39, 31, 23, 15, 7]
 
-# Initial permut made on the key
+# Начальная перестановка ключа
 CP_1 = [57, 49, 41, 33, 25, 17, 9,
         1, 58, 50, 42, 34, 26, 18,
         10, 2, 59, 51, 43, 35, 27,
@@ -18,7 +18,7 @@ CP_1 = [57, 49, 41, 33, 25, 17, 9,
         14, 6, 61, 53, 45, 37, 29,
         21, 13, 5, 28, 20, 12, 4]
 
-# Permut applied on shifted key to get Ki+1
+# Перестановка после сдвига ключа для получения Ki+1
 CP_2 = [14, 17, 11, 24, 1, 5, 3, 28,
         15, 6, 21, 10, 23, 19, 12, 4,
         26, 8, 16, 7, 27, 20, 13, 2,
@@ -26,7 +26,7 @@ CP_2 = [14, 17, 11, 24, 1, 5, 3, 28,
         51, 45, 33, 48, 44, 49, 39, 56,
         34, 53, 46, 42, 50, 36, 29, 32]
 
-# Expand matrix to get a 48bits matrix of datas to apply the xor with Ki
+# Матрица расширения для получения 48-битовой матрицы данных для применения xor с Ki
 E = [32, 1, 2, 3, 4, 5,
      4, 5, 6, 7, 8, 9,
      8, 9, 10, 11, 12, 13,
@@ -38,63 +38,54 @@ E = [32, 1, 2, 3, 4, 5,
 
 # SBOX
 S_BOX = [
-
     [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
      [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
      [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-     [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
-     ],
+     [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
 
     [[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
      [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
      [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
-     [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
-     ],
+     [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]],
 
     [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
      [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
      [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
-     [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
-     ],
+     [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]],
 
     [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
      [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
      [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
-     [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
-     ],
+     [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]],
 
     [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
      [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
      [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
-     [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
-     ],
+     [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]],
 
     [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
      [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
      [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
-     [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
-     ],
+     [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]],
 
     [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
      [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
      [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
-     [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
-     ],
+     [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]],
 
     [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
      [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
      [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-     [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
-     ]
+     [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]],
 ]
 
-# Permut made after each SBox substitution for each round
+# Перестановка после каждой замены S-блока для каждого раунда
 P = [16, 7, 20, 21, 29, 12, 28, 17,
      1, 15, 23, 26, 5, 18, 31, 10,
      2, 8, 24, 14, 32, 27, 3, 9,
      19, 13, 30, 6, 22, 11, 4, 25]
 
-# Final permut for datas after the 16 rounds
+# Конечная перестановка для данных после 16 раундов
 PI_1 = [40, 8, 48, 16, 56, 24, 64, 32,
         39, 7, 47, 15, 55, 23, 63, 31,
         38, 6, 46, 14, 54, 22, 62, 30,
@@ -104,34 +95,33 @@ PI_1 = [40, 8, 48, 16, 56, 24, 64, 32,
         34, 2, 42, 10, 50, 18, 58, 26,
         33, 1, 41, 9, 49, 17, 57, 25]
 
-# Matrix that determine the shift for each round of keys
+# Матрица, определяющая сдвиг для каждого раунда ключей
 SHIFT = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
 
-def string_to_bit_array(text):  # Convert a string into a list of bits
-    array = list()
-    for char in text:
-        binval = binvalue(char, 8)  # Get the char value on one byte
-        array.extend([int(x) for x in list(binval)])  # Add the bits to the final list
-    return array
+def string_to_bit_array(text):
+    return [int(bit) for char in text for bit in '{:08b}'.format(ord(char))]
 
 
-def bit_array_to_string(array):  # Recreate the string from the bit array
-    res = ''.join([chr(int(y, 2)) for y in [''.join([str(x) for x in _bytes]) for _bytes in nsplit(array, 8)]])
-    return res
+def bit_array_to_string(array):
+    return ''.join(chr(int(''.join(map(str, array[i:i+8])), 2)) for i in range(0, len(array), 8))
 
 
-def binvalue(val, bitsize):  # Return the binary value as a string of the given size
-    binval = bin(val)[2:] if isinstance(val, int) else bin(ord(val))[2:]
+def binvalue(val, bitsize): # Возвращает двоичное значение как строку заданного размера
+    if isinstance(val, int):
+        binval = bin(val)[2:]
+    else:
+        binval = bin(ord(val))[2:]
+
+    binval = binval.zfill(bitsize)  # Заполнение нулями до нужной длины
     if len(binval) > bitsize:
-        raise "binary value larger than the expected size"
-    while len(binval) < bitsize:
-        binval = "0" + binval  # Add as many 0 as needed to get the wanted size
+        raise ValueError("binary value larger than the expected size")
+
     return binval
 
 
-def nsplit(s, n):  # Split a list into sublists of size "n"
-    return [s[k:k + n] for k in range(0, len(s), n)]
+def nsplit(s, n):  # Разбиение списка на подсписки размером "n"
+    return [s[i:i + n] for i in range(0, len(s), n)]
 
 
 ENCRYPT = 1
@@ -145,84 +135,94 @@ class Des():
         self.keys = list()
 
     def run(self, key, text, action=ENCRYPT, padding=False):
-        if len(key) < 8:
-            raise "Key Should be 8 bytes long"
-        elif len(key) > 8:
-            key = key[:8]  # If key size is above 8bytes, cut to be 8bytes long
-
-        self.password = key
+        key = self.validate_key(key)
+        self.password = key[:8]
         self.text = text
 
         if padding and action == ENCRYPT:
             self.addPadding()
-        elif len(self.text) % 8 != 0:  # If not padding specified data size must be multiple of 8 bytes
-            raise "Data size should be multiple of 8"
+        elif len(self.text) % 8 != 0: # Если не указана дополнительная информация о размере, размер данных должен быть кратным 8 байтам
+            raise Exception("Data size should be multiple of 8")
 
-        self.generatekeys()  # Generate all the keys
-        text_blocks = nsplit(self.text, 8)  # Split the text in blocks of 8 bytes so 64 bits
-        result = list()
-        for block in text_blocks:  # Loop over all the blocks of data
-            block = string_to_bit_array(block)  # Convert the block in bit array
-            block = self.permut(block, PI)  # Apply the initial permutation
-            g, d = nsplit(block, 32)  # g(LEFT), d(RIGHT)
+        self.generatekeys() # Генерация всех ключей
+        text_blocks = nsplit(self.text, 8) # Разбиение текста на блоки по 8 байт, то есть 64 бита
+        result = []
+
+        for block in text_blocks: # Цикл по всем блокам данных
+            block = string_to_bit_array(block)
+            block = self.permut(block, PI) # Применение начальной перестановки
+            g, d = nsplit(block, 32)
             tmp = None
-            for i in range(16):  # Do the 16 rounds
-                d_e = self.expand(d, E)  # Expand d to match Ki size (48bits)
+
+            for i in range(16): # 16 раундов
+                d_e = self.expand(d, E)   # Расширение d до размера Ki (48 бит)
+
                 if action == ENCRYPT:
-                    tmp = self.xor(self.keys[i], d_e)  # If encrypt use Ki
+                    tmp = self.xor(self.keys[i], d_e) # Если шифрование, использовать Ki
                 else:
-                    tmp = self.xor(self.keys[15 - i], d_e)  # If decrypt start by the last key
-                tmp = self.substitute(tmp)  # Method that will apply the SBOXes
+                    tmp = self.xor(self.keys[15 - i], d_e) # Если дешифрование, начать с последнего ключа
+
+                tmp = self.substitute(tmp) # Применение S-блоков
                 tmp = self.permut(tmp, P)
                 tmp = self.xor(g, tmp)
                 g = d
                 d = tmp
-            result += self.permut(d + g, PI_1)  # Do the last permut and append the result to result
-        final_res = bit_array_to_string(result)
-        if padding and action == DECRYPT:
-            return self.removePadding(final_res)  # Remove the padding if decrypt and padding is true
-        else:
-            return final_res  # Return the final string of data ciphered/deciphered
 
-    def substitute(self, d_e):  # Substitute bytes using SBOX
-        subblocks = nsplit(d_e, 6)  # Split bit array into sublist of 6 bits
-        result = list()
-        for i in range(len(subblocks)):  # For all the sublists
-            block = subblocks[i]
-            row = int(str(block[0]) + str(block[5]), 2)  # Get the row with the first and last bit
-            column = int(''.join([str(x) for x in block[1:][:-1]]), 2)  # Column is the 2,3,4,5th bits
-            val = S_BOX[i][row][column]  # Take the value in the SBOX appropriated for the round (i)
-            bin = binvalue(val, 4)  # Convert the value to binary
-            result += [int(x) for x in bin]  # And append it to the resulting list
+            result += self.permut(d + g, PI_1) # Последняя перестановка и добавление результата
+
+        final_res = bit_array_to_string(result)
+
+        if padding and action == DECRYPT:
+            return self.removePadding(final_res) # Удаление дополнения, если выполняется дешифрование и дополнение включено
+        else:
+            return final_res # Возврат конечной строки данных
+
+    def substitute(self, d_e):  # Цикл по всем подспискам
+        result = []
+
+        for i, block in enumerate(nsplit(d_e, 6)):
+            row = int(str(block[0]) + str(block[5]), 2)  # Получение строки с первым и последним битом
+            column = int(''.join(map(str, block[1:5])), 2)  # Столбец - 2, 3, 4, 5 биты
+            val = S_BOX[i][row][column]  # Получение значения в соответствующем S-блоке (i)
+            result.extend(map(int, binvalue(val, 4)))  # Добавление в результирующий список
+
         return result
 
-    def permut(self, block, table):  # Permut the given block using the given table (so generic method)
+    def permut(self, block, table):  # Перестановка блока по заданной таблице (универсальный метод)
         return [block[x - 1] for x in table]
 
-    def expand(self, block, table):  # Do the exact same thing than permut but for more clarity has been renamed
+    def expand(self, block, table):  # То же, что и permut, но для ясности переименован
         return [block[x - 1] for x in table]
 
-    def xor(self, t1, t2):  # Apply a xor and return the resulting list
+    def xor(self, t1, t2):  # Применение XOR и возврат результата
         return [x ^ y for x, y in zip(t1, t2)]
 
-    def generatekeys(self):  # Algorithm that generates all the keys
+    def validate_key(self, key):
+        if len(key) < 8:
+            raise ValueError("Key should be 8 bytes long")
+        elif len(key) > 8:
+            return key[:8]
+        else:
+            return key
+
+    def generatekeys(self):  # Генерация всех ключей
         self.keys = []
         key = string_to_bit_array(self.password)
-        key = self.permut(key, CP_1)  # Apply the initial permut on the key
-        g, d = nsplit(key, 28)  # Split it in to (g->LEFT),(d->RIGHT)
-        for i in range(16):  # Apply the 16 rounds
-            g, d = self.shift(g, d, SHIFT[i])  # Apply the shift associated with the round (not always 1)
-            tmp = g + d  # Merge them
-            self.keys.append(self.permut(tmp, CP_2))  # Apply the permut to get the Ki
+        key = self.permut(key, CP_1)  # Начальная перестановка ключа
+        g, d = nsplit(key, 28)  # Разделение на (g->LEFT), (d->RIGHT)
+        for i in range(16):  # 16 раундов
+            g, d = self.shift(g, d, SHIFT[i])  # Применение сдвига, соответствующего раунду (не всегда 1)
+            tmp = g + d  # Объединение
+            self.keys.append(self.permut(tmp, CP_2))  # Получение Ki
 
-    def shift(self, g, d, n):  # Shift a list of the given value
+    def shift(self, g, d, n):  # Сдвиг списка на заданное значение
         return g[n:] + g[:n], d[n:] + d[:n]
 
-    def addPadding(self):  # Add padding to the datas using PKCS5 spec.
+    def addPadding(self):  # Добавление дополнения к данным в соответствии с PKCS5
         pad_len = 8 - (len(self.text) % 8)
         self.text += pad_len * chr(pad_len)
 
-    def removePadding(self, data):  # Remove the padding of the plain text (it assume there is padding)
+    def removePadding(self, data):  # Удаление дополнения (предполагается, что оно есть)
         pad_len = ord(data[-1])
         return data[:-pad_len]
 
@@ -239,6 +239,5 @@ if __name__ == '__main__':
     d = Des()
     r = d.encrypt(key, text)
     r2 = d.decrypt(key, r)
-    print(f"Text: {text}, key {key}")
-    print("Ciphered: %r" % r)
-    print("Deciphered: ", r2)
+    print("Encrypted:", r)
+    print("Decrypted:", r2)
--- lab2/kerberos_main.py
+++ lab2/kerberos_main.py
@@ -1,115 +1,95 @@
 import random
 import time
-
 from des_main import Des
 from ast import literal_eval as make_tuple
 
+# Получение текущего времени в миллисекундах
 current_milli_time = lambda: int(round(time.time() * 1000))
+# Преобразование часов в миллисекунды
 hours_to_milli = lambda hour: hour * 3600 * 10000
 
-
-class DesEncrpter:
-    def encrypt(self, data, key):
-        # print()
-        # print('до шифрования:', data)
+class DesEncryptor:
+    def encrypt_data(self, data, key):
         encrypted = Des().encrypt(key=str(key), text=str(data), padding=True)
-        # print('после шифрования:', encrypted)
-        # print()
         return encrypted
 
-    def decrypt(self, data, key):
-        # print()
-        # print('до шифрования:', data)
+    def decrypt_data(self, data, key):
         decrypted = Des().decrypt(key=str(key), text=str(data), padding=True)
         decrypted = make_tuple(decrypted)
-        # print('после шифрования:', decrypted)
-        # print()
         return decrypted
 
-
-class KeyCreator:
+class KeyGenerator:
     @staticmethod
-    def create_key():
+    def generate_key():
         return random.randint(100000000, 999999999)
 
-
-class KDC:
+class KeyDistributionCenter:
+    # Список доступных клиентов и серверов с их ключами
     available_clients = ['client1', 'client2']
-    clients_keys = [KeyCreator.create_key(), KeyCreator.create_key()]
+    clients_keys = [KeyGenerator.generate_key(), KeyGenerator.generate_key()]
     available_servers = ['server1', 'server2']
-    servers_keys = [KeyCreator.create_key(), KeyCreator.create_key()]
+    servers_keys = [KeyGenerator.generate_key(), KeyGenerator.generate_key()]
 
     def __init__(self):
-        self.des = DesEncrpter()
+        self.des = DesEncryptor()
         self.tgs_id = 1
-        self.key_tgs = KeyCreator.create_key()
+        # Генерация ключа Ticket Granting Server (TGS)
+        self.key_tgs = KeyGenerator.generate_key()
 
     def get_permission_ticket(self, client_id):
-        # print('Новый вызов тикета')
-        if client_id in self.available_clients:
-            t = current_milli_time()
-            p = hours_to_milli(48)
-            key_tgs_c = KeyCreator.create_key()
-            ticket = self.build_permission_ticket(client_id, self.tgs_id, t, p, key_tgs_c)
-            # print('Новый вызов тикета:', ticket)
+        if client_id not in self.available_clients:
+            print('Unknown client id')
+            return
 
-            encrypted_ticket = self.des.encrypt(ticket, self.key_tgs)
-            bundle = (encrypted_ticket, key_tgs_c)
+        t = current_milli_time()
+        p = hours_to_milli(48)
+        key_tgs_c = KeyGenerator.generate_key()
+        ticket = self.create_permission_ticket(client_id, self.tgs_id, t, p, key_tgs_c)
 
-            index = self.available_clients.index(client_id)
-            client_key = self.clients_keys[index]
-            encrypted_bundle = self.des.encrypt(bundle, client_key)
+        encrypted_ticket = self.des.encrypt_data(ticket, self.key_tgs)
+        bundle = (encrypted_ticket, key_tgs_c)
 
-            return encrypted_bundle
+        index = self.available_clients.index(client_id)
+        client_key = self.clients_keys[index]
+        encrypted_bundle = self.des.encrypt_data(bundle, client_key)
 
-        print('Неизвестный id клиента')
+        return encrypted_bundle
 
     def get_server_ticket(self, permission_ticket, authority, server_id):
-        # print('Новый вызов тикета')
-        permission_ticket = self.des.decrypt(permission_ticket, self.key_tgs)
-        client_id = permission_ticket[0]
-        t = permission_ticket[2]
-        p = permission_ticket[3]
-        key_tgs_c = permission_ticket[4]
+        permission_ticket = self.des.decrypt_data(permission_ticket, self.key_tgs)
+        client_id, _, t, p, key_tgs_c = permission_ticket
 
-        print(
-            'Данные билета разрешения. id: {}, timestamp: {}, period: {}, key TGS-Client: {}'.format(client_id, t,
-                                                                                                     p, key_tgs_c))
-
-        authority = self.des.decrypt(authority, key_tgs_c)
-        auth_client_id = authority[0]
-        auth_t = authority[1]
-
-        print('Данные авторизации client id: {}, timestamp: {}'.format(auth_client_id, auth_t))
+        authority = self.des.decrypt_data(authority, key_tgs_c)
+        auth_client_id, auth_t = authority
 
         if client_id != auth_client_id:
-            print('Недействительный клиент')
+            print('Invalid client')
             return None
-        if auth_t < t or auth_t > t + p:
-            print('Срок действия истек')
+        if not (t <= auth_t <= t + p):
+            print('Expired')
             return None
 
         t = current_milli_time()
         p = hours_to_milli(48)
-        key_ss_c = KeyCreator.create_key()
-        server_ticket = self.build_server_ticket(client_id, server_id, t, p, key_ss_c)
-        print('Новый тикет сервера:', server_ticket)
+        key_ss_c = KeyGenerator.generate_key()
+        server_ticket = self.create_server_ticket(client_id, server_id, t, p, key_ss_c)
 
         index = self.available_servers.index(server_id)
         server_key = self.servers_keys[index]
-        encrypted_server_ticket = self.des.encrypt(server_ticket, server_key)
+
+        encrypted_server_ticket = self.des.encrypt_data(server_ticket, server_key)
         bundle = (encrypted_server_ticket, key_ss_c)
-        encrypted_bundle = self.des.encrypt(bundle, key_tgs_c)
+        encrypted_bundle = self.des.encrypt_data(bundle, key_tgs_c)
+
         return encrypted_bundle
 
     @staticmethod
-    def build_permission_ticket(client_id, tgs, t, p, key_tgs_c):
+    def create_permission_ticket(client_id, tgs, t, p, key_tgs_c):
         return client_id, tgs, t, p, key_tgs_c
 
     @staticmethod
-    def build_server_ticket(client_id, server_id, t, p, key_ss_c):
+    def create_server_ticket(client_id, server_id, t, p, key_ss_c):
         return client_id, server_id, t, p, key_ss_c
-
 
 class Client:
     def __init__(self, client_id, client_key, kdc, servers):
@@ -117,68 +97,48 @@ class Client:
         self.client_key = client_key
         self.kdc = kdc
         self.servers = servers
-        self.des = DesEncrpter()
+        self.des = DesEncryptor()
         self.permission_ticket = None
         self.key_tgs_c = None
 
     def make_server_call(self, server_number):
-        # print()
-        # print()
-        # print('Вызов сервера', server_number)
         server = self.servers[server_number]
 
         if self.permission_ticket is None or self.key_tgs_c is None:
-            print('Попытка взять билет разрешения')
             permission_ticket_bundle = self.kdc.get_permission_ticket(self.client_id)
             if permission_ticket_bundle is None:
                 return
 
-            permission_ticket_bundle = self.des.decrypt(permission_ticket_bundle, self.client_key)
+            permission_ticket_bundle = self.des.decrypt_data(permission_ticket_bundle, self.client_key)
 
-            permission_ticket = permission_ticket_bundle[0]
-            key_tgs_c = permission_ticket_bundle[1]
-            print('ключ TGS-Client:', key_tgs_c)
+            self.permission_ticket, self.key_tgs_c = permission_ticket_bundle[:2]
 
-            self.permission_ticket = permission_ticket
-            self.key_tgs_c = key_tgs_c
-        else:
-            print('Тикет разрешения и ключ TGS-Client уже определены')
-            permission_ticket = self.permission_ticket
-            key_tgs_c = self.key_tgs_c
+        permission_ticket = self.permission_ticket
+        key_tgs_c = self.key_tgs_c
 
-        print()
-        print('Попытка получить билет на сервер')
-        bundle = self.__call_tgs(permission_ticket, key_tgs_c, server.server_id)
+        bundle = self.request_tgs(permission_ticket, key_tgs_c, server.server_id)
         if bundle is None:
             return
-        bundle = self.des.decrypt(bundle, key_tgs_c)
 
-        server_ticket = bundle[0]
-        key_ss_c = bundle[1]
-        print('Ключ Server-Client:', key_ss_c)
+        server_ticket, key_ss_c = self.des.decrypt_data(bundle, key_tgs_c)
 
-        print()
-        print('Попытка подключения к серверу')
         t = current_milli_time()
         authority = (self.client_id, t)
-        authority_enctypted = self.des.encrypt(authority, key_ss_c)
+        authority_enctypted = self.des.encrypt_data(authority, key_ss_c)
         confirm_t = server.connect(server_ticket, authority_enctypted)
         if confirm_t is None:
             return
-        confirm_t = self.des.decrypt(confirm_t, key_ss_c)
+
+        confirm_t = self.des.decrypt_data(confirm_t, key_ss_c)
         if confirm_t != t + 1:
-            print('Сервер возвращает неправильную метку времени')
+            print('Server returns an incorrect time label')
             return
 
-        print()
-        print('Вызов сервера успешен')
+        print('Server call succesful')
 
-    def __call_tgs(self, permission_ticket, key_tgs_c, server_id):
-        t = current_milli_time()
-        print('Вызов TGS. Server id: {}, timestamp: {}'.format(server_id, t))
-        authority = (self.client_id, t)
-        authority_enctypted = self.des.encrypt(authority, key_tgs_c)
-        bundle = self.kdc.get_server_ticket(permission_ticket, authority_enctypted, server_id)
+    def request_tgs(self, permission_ticket, key_tgs_c, server_id):
+        encrypted_authority = self.des.encrypt_data((self.client_id, current_milli_time()), key_tgs_c)
+        bundle = self.kdc.get_server_ticket(permission_ticket, encrypted_authority, server_id)
         return bundle
 
 
@@ -186,54 +146,38 @@ class Server:
     def __init__(self, server_id, server_key):
         self.server_id = server_id
         self.server_key = server_key
-        self.des = DesEncrpter()
+        self.des = DesEncryptor()
 
     def connect(self, server_ticket, authority):
-        print('Новое подключение сервера')
-        server_ticket = self.des.decrypt(server_ticket, self.server_key)
-        client_id = server_ticket[0]
-        server_id = server_ticket[1]
-        t = server_ticket[2]
-        p = server_ticket[3]
-        key_ss_c = server_ticket[4]
+        client_id, received_server_id, t, p, key_ss_c = self.des.decrypt_data(server_ticket, self.server_key)
 
-        print('Данные тикета сервера. Client id: {}, timestamp: {}, period: {}, key Server-Client: {}'.format(client_id,
-                                                                                                              t,
-                                                                                                              p,
-                                                                                                              key_ss_c))
-
-        if server_id != self.server_id:
-            print('Неверный сервер')
+        if received_server_id != self.server_id:
+            print('Wrong server')
             return None
 
-        authority = self.des.decrypt(authority, key_ss_c)
-        auth_client_id = authority[0]
-        auth_t = authority[1]
-
-        print('Данные авторизациисд. Client id: {}, timestamp: {}'.format(auth_client_id, auth_t))
+        auth_client_id, auth_t = self.des.decrypt_data(authority, key_ss_c)
 
         if client_id != auth_client_id:
             print('Invalid client')
             return None
-        if auth_t < t or auth_t > t + p:
+        if not (t <= auth_t <= t + p):
             print('Ticket is expired')
             return None
 
         confirm_t = auth_t + 1
-        print('Confirmation timestamp is', confirm_t)
-        encrypted_confirm_t = self.des.encrypt(confirm_t, key_ss_c)
+        encrypted_confirm_t = self.des.encrypt_data(confirm_t, key_ss_c)
         return encrypted_confirm_t
 
 
 def init():
-    kdc = KDC()
-    server1 = Server(kdc.available_servers[0], kdc.servers_keys[0])
-    server2 = Server(kdc.available_servers[1], kdc.servers_keys[1])
-    client = Client(kdc.available_clients[0], kdc.clients_keys[0], kdc, [server1, server2])
+    kdc = KeyDistributionCenter()
+    servers = [Server(sid, skey) for sid, skey in zip(kdc.available_servers, kdc.servers_keys)]
+    client = Client(kdc.available_clients[0], kdc.clients_keys[0], kdc, servers)
 
-    print('server0 id: {}, server0 key: {}'.format(server1.server_id, server1.server_key))
-    print('server1 id: {}, server1" key: {}'.format(server2.server_id, server2.server_key))
-    print('Client id: {}, Client key: {}'.format(client.client_id, client.client_key))
+    for idx, server in enumerate(servers):
+        print(f'Server{idx} id: {server.server_id}, Server{idx} key: {server.server_key}')
+
+    print(f'Client id: {client.client_id}, Client key: {client.client_key}')
 
     return client
 
@@ -241,10 +185,6 @@ def init():
 def main():
     client = init()
     client.make_server_call(0)
-
-
-# client.make_server_call(1)
-
 
 if __name__ == "__main__":
     main()
